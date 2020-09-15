e11a4c1d18bc30e95f4e067ade95a6301792f333
Nixon.Shih <powerwolf543@gmail.com>
2020-09-15 22:18:20 +0800

    Add new module of `Search`

---
+++ Search/Sources/Search/Search.swift
@@ -0,0 +1,6 @@
+//
+//  Search.swift
+//  Created by Nixon Shih on 2020/9/15.
+//
+
+import Foundation
---
+++ Search/Tests/SearchTests/SearchTests.swift
@@ -0,0 +1,6 @@
+//
+//  SearchTests.swift
+//  Created by Nixon Shih on 2020/9/15.
+//
+
+import Foundation
